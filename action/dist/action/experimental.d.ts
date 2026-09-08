415942c20113aa3696df1907d4d3260a23cae4ee
Englios <englios656@gmail.com>
2026-09-08 15:21:21 +0800

    chore(action): stop tracking generated bundle

--- action/dist/action/experimental.d.ts
+++
@@ -1,7 +0,0 @@
-interface ActionExperimentalCore {
-    getInput(name: string): string;
-    getBooleanInput(name: string): boolean;
-}
-export declare function experimentalFromActionInput(core: ActionExperimentalCore): boolean | undefined;
-export {};
-//# sourceMappingURL=experimental.d.ts.map
--- action/dist/src/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { Hono } from 'hono';
-declare const server: Hono<import("hono/types").BlankEnv, import("hono/types").BlankSchema, "/">;
-export { server };
-//# sourceMappingURL=index.d.ts.map
